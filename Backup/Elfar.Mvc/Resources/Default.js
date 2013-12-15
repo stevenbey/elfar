d78bcbf5eeeab2c8452cd73d38bab02e4310c990
Steven Bey <github@stevenbey.com>
2013-12-15 21:14:16 +0000

    Removed Backup directory

--- Backup/Elfar.Mvc/Resources/Default.js
+++
@@ -1,1 +0,0 @@
-﻿function window_height(){return $(window).height()*.94}function dialog_resize(){$("#dialog").dialog("option","height",window_height())}$(function(){dialog_resize();$("#show-html").click(dialog_open);$("#tabs").tabs();$(".ui-tabs-nav a").focus(function(){this.blur()});$(window).resize(dialog_resize)})
--- Backup/Elfar.Web/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Elfar.Web.Global" Language="C#" %>
