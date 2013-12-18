345161252ee22abe4f0c8cd8ebb677203ba0bf57
Steven Bey <github@stevenbey.com>
2013-12-18 18:02:04 +0000

    Added SQLite provider.

--- Elfar.Data.SQLite/SQLiteErrorLogProvider.cs
+++ Elfar.Data.SQLite/SQLiteErrorLogProvider.cs
@@ -1,52 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.SQLite;
-using System.IO;
-using System.Linq;
+﻿using System.Data.SQLite;
 
 namespace Elfar.Data.SQLite
 {
-    using Properties;
-
-    public class SQLiteErrorLogProvider : FileBasedDbErrorLogProvider<SQLiteConnection>
-    {
-        public SQLiteErrorLogProvider()
-        {
-            if (File.Exists(FilePath)) return;
-            lock (key)
-            {
-                if (File.Exists(FilePath)) return;
-                TryExecute(() =>
-                {
-                    SQLiteConnection.CreateFile(FilePath);
-                    using (var conn = Connection)
-                    {
-                        conn.Execute(Resources.Table);
-                    }
-                });
-            }
-        }
-
-        public override ErrorLog Get(Guid id)
-        {
-            using(var conn = Connection)
-            {
-                return conn.Query<SQLiteErrorLog>(Queries.Get, new { ID = id }).SingleOrDefault();
-            }
-        }
-        public override IList<ErrorLog> List()
-        {
-            using(var conn = Connection)
-            {
-                return new List<ErrorLog>(conn.Query<SQLiteErrorLog>(Queries.List, new { Application }).Select(l => (ErrorLog) l));
-            }
-        }
-
-        protected override string DefaultConnectionString
-        {
-            get { return @default; }
-        }
-
-        const string @default = @"|DataDirectory|\Elfar.db";
-    }
+    public class SQLiteErrorLogProvider : DbErrorLogProvider<SQLiteConnection> {}
 }
