c0e611885da146d9f89855243678465f5f3e0c96
Steven Bey <github@stevenbey.com>
2013-12-23 19:02:11 +0000

    Added Oracle provider project.

---
+++ Elfar.Data.Oracle/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Elfar.Data.Oracle")]
+[assembly: AssemblyDescription("Oracle ErrorLog Provider for ELFAR.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Beyond395 Limited")]
+[assembly: AssemblyProduct("Elfar.Data.Oracle")]
+[assembly: AssemblyCopyright("Copyright © Beyond395 Limited 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("759acfa9-38b6-4b01-852d-cbc69e5c27c5")]
+[assembly: AssemblyVersion("2.0.*")]
+[assembly: AssemblyFileVersion("2.0")]
