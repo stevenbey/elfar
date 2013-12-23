408588bfd79d1e7eedbe4fda03ed9d5477bcc4e7
Steven Bey <github@stevenbey.com>
2013-12-23 18:58:50 +0000

    Added PostgreSQL provider.

---
+++ Elfar.Data.PostgreSQL/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Elfar.Data.PostgreSQL")]
+[assembly: AssemblyDescription("PostgreSQL ErrorLog Provider for ELFAR.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Beyond395 Limited")]
+[assembly: AssemblyProduct("Elfar.Data.PostgreSQL")]
+[assembly: AssemblyCopyright("Copyright © Beyond395 Limited 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("95daa96f-27c4-4b1e-9ef3-d0a35215aefa")]
+[assembly: AssemblyVersion("2.0.*")]
+[assembly: AssemblyFileVersion("2.0")]
