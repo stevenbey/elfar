885f935f0e0c446f2f5289c631fef627a9a1f1ec
Steven Bey <github@stevenbey.com>
2013-12-23 19:04:18 +0000

    Added MySQL provider project.

---
+++ Elfar.Data.MySQL/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Elfar.Data.MySQL")]
+[assembly: AssemblyDescription("MySQL ErrorLog Provider for ELFAR.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Beyond395 Limited")]
+[assembly: AssemblyProduct("Elfar.Data.MySQL")]
+[assembly: AssemblyCopyright("Copyright © Beyond395 Limited 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("e53592b8-8d4d-4bcb-a27c-2b6aef93b62e")]
+[assembly: AssemblyVersion("2.0.*")]
+[assembly: AssemblyFileVersion("2.0")]
