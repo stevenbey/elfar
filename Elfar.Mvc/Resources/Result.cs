a23cc60d76bf6382fa0e441b89e649647c2bba73
Steven Bey <github@stevenbey.com>
2014-04-08 11:53:15 +0100

    Added caching of the resource names.

--- Elfar.Mvc/Resources/Result.cs
+++ Elfar.Mvc/Resources/Result.cs
@@ -1,20 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Web.Mvc;
 
 namespace Elfar.Mvc.Resources
 {
+    // ReSharper disable PossibleNullReferenceException
     internal class Result : FileStreamResult
     {
+        static Result()
+        {
+            names = assembly.GetManifestResourceNames().GroupBy(n => Path.GetExtension(n).Trim('.'), Path.GetFileNameWithoutExtension);
+        }
+
         public Result(string filename, string ext) : base(GetStream(filename, ext), "text/" + ext) { }
 
         static Stream GetStream(string filename, string ext)
         {
-            var assembly = typeof(Result).Assembly;
-            var resourceName = assembly.GetManifestResourceNames().FirstOrDefault(n => n.EndsWith(filename + "." + ext, StringComparison.OrdinalIgnoreCase));
-            var s = resourceName == null ? null : assembly.GetManifestResourceStream(resourceName);
-            return s;
+            var group = names.FirstOrDefault(g => g.Key == ext);
+            if (group != null)
+            {
+                var resourceName = group.FirstOrDefault(f => f.EndsWith(filename, StringComparison.OrdinalIgnoreCase));
+                if (resourceName != null) return assembly.GetManifestResourceStream(resourceName + "." + ext);
+            }
+            return null;
         }
+
+        static readonly Assembly assembly = typeof(Result).Assembly;
+        static readonly IEnumerable<IGrouping<string, string>> names;
     }
 }
