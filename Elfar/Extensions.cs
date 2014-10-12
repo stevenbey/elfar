0791c43e4cb1b4c4e4460d6e8d21f235a79d4cc1
Steven Bey <github@stevenbey.com>
2014-10-12 15:22:07 +0100

    Refactoring

--- Elfar/Extensions.cs
+++ Elfar/Extensions.cs
@@ -8,28 +8,37 @@ namespace Elfar
 {
     static class Extensions
     {
+        public static string ToPascal(this object value)
+        {
+            var s = value as string;
+            return string.IsNullOrWhiteSpace(s) ? null : Regex.Replace(s, @"^[a-z]", m => m.Value.ToUpper());
+        }
+    }
+
+    static class GZip
+    {
         public static string Compress(this string value)
         {
-            using(var msi = new MemoryStream(Encoding.UTF8.GetBytes(value)))
-            using(var mso = new MemoryStream())
+            using (var msi = new MemoryStream(Encoding.GetBytes(value)))
+            using (var mso = new MemoryStream())
             {
-                using(var gs = new GZipStream(mso, CompressionMode.Compress)) msi.CopyTo(gs);
+                using (var gs = new GZipStream(mso, CompressionMode.Compress)) msi.CopyTo(gs);
                 return Convert.ToBase64String(mso.ToArray());
             }
         }
         public static string Decompress(this string value)
         {
             using (var msi = new MemoryStream(Convert.FromBase64String(value)))
-            using(var mso = new MemoryStream())
+            using (var mso = new MemoryStream())
             {
-                using(var gs = new GZipStream(msi, CompressionMode.Decompress)) gs.CopyTo(mso);
-                return Encoding.UTF8.GetString(mso.ToArray());
+                using (var gs = new GZipStream(msi, CompressionMode.Decompress)) gs.CopyTo(mso);
+                return Encoding.GetString(mso.ToArray());
             }
         }
-        public static string ToPascal(this object value)
+
+        static Encoding Encoding
         {
-            var s = value as string;
-            return string.IsNullOrWhiteSpace(s) ? null : Regex.Replace(s, @"^[a-z]", m => m.Value.ToUpper());
+            get { return Encoding.UTF8; }
         }
     }
 }
