0ebc1deb0c23a3b417d8045bc526086cfca4fee4
stevenbey <github@stevenbey.com>
2015-04-22 13:49:25 +0100

    Updated the SQL script to reflect the removal of dashes from the ID.

--- Elfar.Data/Elfar_ErrorLogs.sql
+++ Elfar.Data/Elfar_ErrorLogs.sql
@@ -1,6 +1,6 @@
 CREATE TABLE [Elfar_ErrorLogs]
 (
-	[ID]		[char](36) NOT NULL PRIMARY KEY,
+	[ID]		[char](32) NOT NULL PRIMARY KEY,
 	[Summary]	[varchar](MAX) NOT NULL,
 	[Detail]	[varchar](MAX) NOT NULL
 )
